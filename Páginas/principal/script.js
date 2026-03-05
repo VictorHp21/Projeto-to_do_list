9a7a0f56ec507b17a8ed2143586aefbeb1e72bc0
Victor Henrique <102931817+VictorHp21@users.noreply.github.com>
2026-03-05 20:04:25 -0300

    Adicionando tela de tarefas, primeira logica e estilo base

---
+++ Páginas/principal/script.js
@@ -0,0 +1,15 @@
+
+function mostrarTarefa(){
+     document.getElementById('taskArea').classList.remove('hide')
+    document.getElementById('dataArea').classList.remove('hide')
+
+    document.getElementById('btn-add').style.display = 'none'
+   
+}
+
+function voltaTarefa(){
+    document.getElementById('taskArea').classList.add('hide')
+    document.getElementById('dataArea').classList.add('hide')
+
+    document.getElementById('btn-add').style.display = 'flex'
+}
