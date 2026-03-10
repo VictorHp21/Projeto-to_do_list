f34de3829525626e9e6d76b0c86571808aab1fc2
Victor Henrique <102931817+VictorHp21@users.noreply.github.com>
2026-03-09 23:23:57 -0300

    Estrutura visual base e logica de click do botao status feita

--- Páginas/principal/script.js
+++ Páginas/principal/script.js
@@ -1,19 +1,69 @@
 
-function mostrarTarefa(){
+
+
+// FUnções do botao de mostrar tarefa
+
+function mostrarTarefa() {
 
     document.getElementById('title').classList.remove('hide')
 
-     document.getElementById('taskArea').classList.remove('hide')
+    document.getElementById('taskArea').classList.remove('hide')
     document.getElementById('dataArea').classList.remove('hide')
 
     document.getElementById('btn-add').style.display = 'none'
-   
+
 }
 
-function voltaTarefa(){
+function voltaTarefa() {
     document.getElementById('taskArea').classList.add('hide')
     document.getElementById('dataArea').classList.add('hide')
     document.getElementById('title').classList.add('hide')
 
     document.getElementById('btn-add').style.display = 'flex'
+}
+
+
+// Funções do bt status
+
+function mostrarMenuStatus(botao) {
+
+
+
+    const container = botao.closest('.task-status')
+
+    const menu = container.querySelector('.status-menu')
+
+    menu.classList.toggle('hide')
+}
+
+function setStatus(element, status){
+
+    const container = element.closest('.task-status')
+
+    const button = container.querySelector('.btn-status')
+
+    const text = container.querySelector('.task-status-text')
+
+    button.classList.remove('pendente','feito','atraso')
+
+    button.classList.add(status)
+
+       if(status === 'feito'){
+        text.innerHTML = 'Finalizado'
+    }
+
+    if(status === 'pendente'){
+        text.innerHTML = 'Pendente'
+    }
+
+    if(status === 'atraso'){
+        text.innerHTML = 'Em atraso'
+    }
+
+    container.querySelector('.status-menu').classList.add('hide')
+
+    
+
+    
+
 }
