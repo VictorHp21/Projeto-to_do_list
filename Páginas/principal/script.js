2bafefd8c7e3aa3575ef648f15f99bcd7de70a50
Victor Henrique <102931817+VictorHp21@users.noreply.github.com>
2026-03-09 21:24:09 -0300

    adicionando espaço para titulo da tarefa e iniciando configurações visuais do espaço suas tarefas

--- Páginas/principal/script.js
+++ Páginas/principal/script.js
@@ -1,5 +1,8 @@
 
 function mostrarTarefa(){
+
+    document.getElementById('title').classList.remove('hide')
+
      document.getElementById('taskArea').classList.remove('hide')
     document.getElementById('dataArea').classList.remove('hide')
 
@@ -10,6 +13,7 @@ function mostrarTarefa(){
 function voltaTarefa(){
     document.getElementById('taskArea').classList.add('hide')
     document.getElementById('dataArea').classList.add('hide')
+    document.getElementById('title').classList.add('hide')
 
     document.getElementById('btn-add').style.display = 'flex'
 }
