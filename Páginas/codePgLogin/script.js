538c4b05c96d726774302dfc998fc038cedbfc96
Victor Henrique <102931817+VictorHp21@users.noreply.github.com>
2026-03-10 21:47:57 -0300

    logica em js da tela de cadastro

--- Páginas/codePgLogin/script.js
+++ Páginas/codePgLogin/script.js
@@ -1,11 +1,72 @@
- const section = document.querySelector("section#login");
-    const btnCadastrar = document.querySelector("#btnCadastrar");
-    const btnLogar = document.querySelector("#btnLogar");
+const section = document.querySelector("section#login");
+const btnCadastrar = document.querySelector("#btnCadastrar");
+const btnLogar = document.querySelector("#btnLogar");
 
-    btnCadastrar.addEventListener("click", () => {
-        section.classList.add("active");
-    });
+btnCadastrar.addEventListener("click", () => {
+    section.classList.add("active");
+});
 
-    btnLogar.addEventListener("click", () => {
-        section.classList.remove("active");
-         });
+btnLogar.addEventListener("click", () => {
+    section.classList.remove("active");
+});
+
+
+
+// para enviar os dados de cadastro
+
+document.getElementById("formulario-cadastro").addEventListener("submit", async function (e) {
+    e.preventDefault() // impedir recarregamento da pag ao env cadas
+
+    const nome = document.getElementById("nome").value
+    const email = document.getElementById("email").value
+    const senha = document.getElementById("senha").value
+
+      if(!nome || !email || !senha){
+        alert("Preencha todos os campos")
+        return
+    }
+
+    if(!email.includes("@")){
+    alert("Digite um email válido")
+    return
+}
+
+    const usuario = {
+        nome: nome,
+        email: email,
+        senha: senha
+    }
+
+    try {
+        const resposta = await fetch("http://localhost:8080/usuarios/cadastro", {
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify(usuario)
+        })
+
+        if (resposta.ok){
+           // alert("Usuário cadastrado")
+
+            document.getElementById("formulario-cadastro").reset()
+
+            window.location.href = "../principal/main.html"
+
+
+        } else {
+            alert("Erro ao cadastrar")
+        }
+    } catch(erro){
+       console.error(erro)
+
+        alert("Erro na api")
+    }
+
+   
+
+   // const dados = await resposta.json()
+
+})
